ef643c733840364a5d9e156e80a65edbd3ed942b
tildanyrvik <tilda.nyrvik@gmail.com>
2026-05-05 20:00:31 +0200

    button created

--- mini-game/src/Components/API.jsx
+++ mini-game/src/Components/API.jsx
@@ -1,21 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function API () {
+    const [dogImage, setDogImage] = useState([ ]);
     const fetchData = async () => {
         try {
             const response = await fetch("https://dog.ceo/api/breeds/image/random");
             const data = await response.json();
-            console.log(data);
+            setDogImage(data);
         }
         catch (error){
             console.log("Error fetching data:", error);
         };
     };
     useEffect(() => {fetchData();}, []);
+    const generateImage = () => {
+        fetchData();
+    }
 
     return(
         <div>
-
+            <p>Message: {dogImage.message}</p>
+            <br />
+            <button oncClick={generateImage}>Generate new image</button>
         </div>
     );
 };
