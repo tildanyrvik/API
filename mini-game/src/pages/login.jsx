58d2d6c33945453dbb2d44d92da0a51ec0184967
tildanyrvik <tilda.nyrvik@gmail.com>
2026-05-10 23:28:55 +0200

    login created

--- mini-game/src/pages/login.jsx
+++ mini-game/src/pages/login.jsx
@@ -2,26 +2,27 @@ import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 
 function Login (){
-    const [userName, setUsername] = useState("");
+    const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
-
     const navigate = useNavigate();
 
     const processLogin = () => {
         const storedPassword = localStorage.getItem(username);
+    
+    if (storedPassword === password) {
+         navigate("/game");
+    }else {
+        alert("Wrong username or password");
+    }
+    }
 
-        if (storedPassword === null) {
-            alert("Username not found");
-        } else if (storedPassword === password) {
-            navigate("/game");
-        } else {
-            alert("Wrong password");
-        }
-    };
+
 
     return(
         <div>
-            <h1>login</h1>
+            <input type="text" placeholder="Username" value={username} onChange= {(e) => setUsername(e.target.value)} />
+            <input type="password" placeholder="Password" value={password} onChange= {(e) => setPassword(e.target.value)} />
+            <button onClick={processLogin}>Sign in</button>
         </div>
     )
 }
